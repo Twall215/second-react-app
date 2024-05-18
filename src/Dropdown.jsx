f9d9b5743dd0e7ac86d039e5579d971d803b9422
Trey <contacttreyw@gmail.com>
2024-05-18 15:22:21 -0500

    Drop down jsx added

---
+++ src/Dropdown.jsx
@@ -0,0 +1,14 @@
+function Dropdown (){
+    return (
+        <div className = "dropdown">
+            <div className = "dropdown-btn">Choose One</div>
+            <div className = "dropdown-content">
+                <div className = "dropdown-item">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Dropdown
