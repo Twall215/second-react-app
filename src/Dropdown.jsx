64a9e3cbed6b4679474f36d99676258c503739f9
Trey <contacttreyw@gmail.com>
2024-05-21 16:46:14 -0500

    adding the currencies to the dropdown

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -4,7 +4,94 @@ function Dropdown (){
             <div className = "dropdown-btn">Choose One</div>
             <div className = "dropdown-content">
                 <div className = "dropdown-item">
-
+                    AUD
+                </div>
+                <div className = "dropdown-item">
+                    BGN
+                </div>
+                <div className = "dropdown-item">
+                    BRL
+                </div>
+                <div className = "dropdown-item">
+                    CAD
+                </div>
+                <div className = "dropdown-item">
+                    CHF
+                </div>
+                <div className = "dropdown-item">
+                    CNY
+                </div>
+                <div className = "dropdown-item">
+                    CZK
+                </div>
+                <div className = "dropdown-item">
+                    DKK
+                </div>
+                <div className = "dropdown-item">
+                    EUR
+                </div>
+                <div className = "dropdown-item">
+                    HKD
+                </div>
+                <div className = "dropdown-item">
+                    HUF
+                </div>
+                <div className = "dropdown-item">
+                    IDR
+                </div>
+                <div className = "dropdown-item">
+                    ILS
+                </div>
+                <div className = "dropdown-item">
+                    INR
+                </div>
+                <div className = "dropdown-item">
+                    ISK
+                </div>
+                <div className = "dropdown-item">
+                    JPY
+                </div>
+                <div className = "dropdown-item">
+                    KRW
+                </div>
+                <div className = "dropdown-item">
+                    MXN
+                </div>
+                <div className = "dropdown-item">
+                    MYR
+                </div>
+                <div className = "dropdown-item">
+                    NOK
+                </div>
+                <div className = "dropdown-item">
+                    NZD
+                </div>
+                <div className = "dropdown-item">
+                    PHP
+                </div>
+                <div className = "dropdown-item">
+                    PLN
+                </div>
+                <div className = "dropdown-item">
+                    RON
+                </div>
+                <div className = "dropdown-item">
+                    SEK
+                </div>
+                <div className = "dropdown-item">
+                    SGD
+                </div>
+                <div className = "dropdown-item">
+                    THB
+                </div>
+                <div className = "dropdown-item">
+                    TRY
+                </div>
+                <div className = "dropdown-item">
+                    USD
+                </div>
+                <div className = "dropdown-item">
+                    ZAR
                 </div>
             </div>
         </div>
