594e51c7503a2dc3c6939998d5650c7b47974e3f
Trey Wallace <contacttreyw@gmail.com>
2024-08-07 20:54:09 -0500

    Almost functional

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Dropdown from './Dropdown';
 
 function CurrencyConverter() {
   const [currencyOptions, setCurrencyOptions] = useState([])
-  const [fromCurrency, setFromCurrency] = useState("USD")
-  const [toCurrency, setToCurrency] = useState("AUD")
+  const [fromCurrency, setFromCurrency] = useState('USD')
+  const [toCurrency, setToCurrency] = useState('AUD')
   const [exchangeRate, setExchangeRate] = useState()
   const [amount, setAmount] = useState(1)
   const[amountInFromCurrency, setAmountInFromCurrency] = useState(true)
@@ -16,14 +16,12 @@ function CurrencyConverter() {
   let toAmount, fromAmount
   if(amountInFromCurrency){
     fromAmount = amount
-    toAmount = amount*exchangeRate
+    toAmount = amount*exchangeRate[toCurrency]
   } else{
     toAmount = amount
     fromAmount = amount/exchangeRate
   }
 
-  //console.log(exchangeRate)
-  
   useEffect(() => {
     fetch(Base_URL+fromCurrency)
     .then(res => res.json())
@@ -32,7 +30,24 @@ function CurrencyConverter() {
       setExchangeRate(data.rates)
     })
   }, [fromCurrency])
+
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+      fetch(`${Base_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
+        .then(res => res.json())
+        .then(data => setExchangeRate(data.rates))
+    }
+  }, [fromCurrency, toCurrency])
     
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+  }
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+  }
 
     return (
       
@@ -45,12 +60,16 @@ function CurrencyConverter() {
             currencyOptions = {currencyOptions}
             selectedCurrency={fromCurrency}
             onChangeCurrency={e => setFromCurrency(e.target.value)} //<- supposed to take the target event(currency selection) and change it to that option
+            onChangeAmount = {handleFromAmountChange}
+            amount = {fromAmount}
             />
-          <div className="equals">{amount}={amount*exchangeRate[toCurrency]}</div>
+          <div className="equals">{fromCurrency} {amount}={amount * exchangeRate[toCurrency]} {toCurrency}</div>
           <Dropdown
           currencyOptions = {currencyOptions}
           selectedCurrency={toCurrency}
           onChangeCurrency={e => setToCurrency(e.target.value)} //<- supposed to take the target event(currency selection) and change it to that option
+          onChangeAmount = {handleToAmountChange}
+          amount = {toAmount}
             />
         </div>
       </div>
--- src/Dropdown.js
+++ src/Dropdown.js
@@ -5,12 +5,13 @@ export default function CurrencyRow(props){
         currencyOptions,
         selectedCurrency,
         onChangeCurrency,
-        // amount
+        onChangeAmount,
+        amount
     } = props
 
     return(
         <div>
-            <input type="number" className= "input" /*value = {amount}*//>
+            <input type="number" className= "input" value = {amount} onChange={onChangeAmount}/>
             <select value = {selectedCurrency} onChange = {onChangeCurrency} >
                 {currencyOptions.map(option => (
                     <option key={option} value = {option}>{option}</option>
